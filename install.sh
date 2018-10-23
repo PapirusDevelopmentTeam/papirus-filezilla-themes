ccdb1d7059ffec5bc795bb169a30235885978bd9
Sergei Eremenko <finalitik@gmail.com>
2018-10-24 00:49:51 +0300

    Minor fix to install.sh

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ cat <<- EOF
 
 EOF
 
-: "${THEMES:=papirus epapirus papirus-dark papirus-adapta papirus-adapta-nokto}"
 : "${DESTDIR:=/usr/share/filezilla/resources}"
 : "${TAG:=master}"
+: "${THEMES:=papirus epapirus papirus-dark papirus-adapta papirus-adapta-nokto}"
 : "${uninstall:=false}"
 
 _msg() {
@@ -50,7 +50,7 @@ _sudo() {
 _download() {
     _msg "Getting the latest version from GitHub ..."
     wget -O "$temp_file" \
-        "https://github.com/PapirusDevelopmentTeam/$gh_repo/archive/master.tar.gz"
+        "https://github.com/PapirusDevelopmentTeam/$gh_repo/archive/$TAG.tar.gz"
     _msg "Unpacking archive ..."
     tar -xzf "$temp_file" -C "$temp_dir"
 }
